e269a3a78e3552b84c10b94e229d6ab92fedbd89
SsongCh94 <thddkwl94@gmail.com>
2023-03-28 16:25:38 +0900

    실수... dev에서 작업해버렸습니다

--- src/pages/KakaoPage.jsx
+++ src/pages/KakaoPage.jsx
@@ -4,7 +4,7 @@ import React, { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 function KakaoPage() {
-  // const navigation = useNavigate();
+  const navigation = useNavigate();
 
   // useEffect(() => {
   //   const code = new URL(window.location.href).searchParams.get('code')
@@ -14,8 +14,9 @@ function KakaoPage() {
 
   // const { mutate } = useMutation({
   //   mutationFn: async (payload) => {
-  //     const result = await axios.post('', { code: payload })
-  //     console.log(result);
+  //     const response = await axios.get(`http://43.201.98.79:8080/api/user/kakao/callback?code=${payload}`)
+  //     console.log(response);
+  //     return response
   //   },
   //   onSuccess: () => {
   //     window.alert('성공!')
